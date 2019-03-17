610025aa60355514427b28793e6a85050c73f0e4
wenjie.fan <fanwenjie415@pingan.com.cn>
2019-03-17 23:26:54 +0800

    709

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # al-pj
 aims to improve algorithm ability base on leetcode
+
+
---
+++ src/lc/seven/ToLowerCase.java
@@ -0,0 +1,61 @@
+package lc.seven;
+
+/**
+ * TODO description
+ *
+ * @author fanwenjie415
+ * @date 2019-03-17
+ * @since v1.0.0
+ */
+public class ToLowerCase  {
+    public static void main(String[] args) {
+        String[] case1J = new String[]{"Hello", "here", "LOVELY"};
+
+        long st = System.currentTimeMillis();
+
+        final Solution2 solution = new Solution2();
+        for (int i = 0; i < 1; i++) {
+
+            for (String data : case1J) {
+                System.out.println("Output: " +solution.toLowerCase(data));
+            }
+        }
+        long et = System.currentTimeMillis();
+        System.out.println("cost : " + (et - st) + " ms");
+    }
+}
+
+class Solution1 {
+    public String toLowerCase(String str) {
+
+        final char[] chars = str.toCharArray();
+        String sb = "";
+        for (int i = 0; i < chars.length; i++) {
+            if (65 <= chars[i] && chars[i] < 91) {
+                chars[i] += 32;
+            }
+
+            sb += chars[i];
+        }
+
+        return sb;
+    }
+}
+
+
+class Solution2 {
+    public String toLowerCase(String str) {
+
+        final char[] chars = str.toCharArray();
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            if (65 <= chars[i] && chars[i] < 91) {
+                chars[i] += 32;
+            }
+
+            sb.append(chars[i]);
+        }
+
+        return sb.toString();
+    }
+}
